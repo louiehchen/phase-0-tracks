160658d779606906bf2865d83fae6727645b0ee7
Louie Chen <louiehchen@gmail.com>
2016-09-15 14:27:02 -0700

    Add vampire assignment

---
+++ ruby/vampires.rb
@@ -0,0 +1,48 @@
+puts "How many employees are being processed?"
+employees = gets.to_i
+
+until employees == 0
+
+	puts "What is your name?"
+	name = gets.chomp
+	puts "How old are you?"
+	age = gets.to_i
+	puts "What year were you born?"
+	year = gets.to_i
+	puts "Our company cafeteria serves garlic bread. Should we order some for you? (Y/N)"
+	bread = gets.chomp
+	puts "Would you like to enroll in the company's health insurance? (Y/N)"
+	health = gets.chomp
+	puts "Do you have any allergies (Y/N)?"
+	allergies = gets.chomp
+		
+	
+	
+
+	current_year = 2016
+	
+	if name == "Drake Cula" || name == "Tu Fang"
+		result = "Definitely a vampire"
+	elsif (age == current_year - year && (bread == "Y" || health == "Y"))
+		result = "Probably not a vampire"
+	elsif (age != current_year- year && (bread == "N" || health == "N"))
+		result = "Probably a vampire"
+	elsif (age != current_year - year && bread == "N" && health == "N")
+		result = "Almost certainly a vampire"
+	elsif allergies == "Y"
+			user_input = ""
+			while user_input != "Done"
+			puts "List an allergy you have, type 'Done' when you're finished"
+			user_input = gets.chomp
+			end
+			if user_input = "Sunshine"
+				result = "Probably a vampire"
+			end
+	else
+		result = "Results inconclusive"
+
+	end
+	puts result
+	employees = employees - 1	
+end
+puts "Actually, nevermind! What do these questions have to do with anything? Let's all be friends."
