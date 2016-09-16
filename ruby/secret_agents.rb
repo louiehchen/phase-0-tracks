a0eecf930e3f837f0b77b5c9ea7c20b2d3b6144d
Louie Chen <louiehchen@gmail.com>
2016-09-16 13:28:17 -0700

    final release

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -2,29 +2,36 @@
 # Within the encrypt method, write an algorithm that takes a string and advances each letter one index forward. Within the decrypt method, write an algorithm that reverses the given string one character. 
 
 def encrypt(str)
+
  index = 0 
- letters = "abcdefghijklmnopqrstuvwxyz"
-	while index < str.length 
-		if str[0] == letters[-1]
-		 	str[index] = letters[index]
-		  	str[index].next!
-		elsif str[index] == " "
-  			str[index] = " "
-		else 
-		  	str[index] = str[index].next
-		end
-	index+=1
-	end
+ charset = "abcdefghijklmnopqrstuvwxyz"
+  while index < str.length 
+  if str[0] == charset[-1]
+		str[index] = charset[index]
+		str[index].next!
+  elsif str[index] == " "
+  	str[index] = " "
+  else 
+  	str[index] = str[index].next!
+  end
+  index += 1
+  end
+   
+ 
+  
+ 	
 
 puts str
-
 end
-encrypt("zed")
+
+encrypt("z d")
+
 def decrypt(str)
+	index = 0
 	charset = "abcdefghijklmnopqrstuvwxyz"
 	while index < str.length
-		if str[index] == "z"
-			str[index] = "a"
+		if  str[index] == charset[0]
+			 str[index] = charset[-1]
 		elsif str[index] == " "
 			str[index] = " "
 		else 
@@ -32,5 +39,22 @@ def decrypt(str)
 		end
 		index += 1 
 	end
-	puts str 
+puts str 
 end 
+
+decrypt("afe")
+decrypt("hello world")
+
+
+#decrypt(encrypt("swordfish")) returns the user input because of implicit return 
+
+puts "Greetings, agent! Do you want to encrypt or decrypt your password?"
+    encrypt = gets.chomp == "encrypt"
+
+puts "Password?"
+
+if encrypt
+    p encrypt(gets.chomp)
+else
+    p decrypt(gets.chomp)
+end
