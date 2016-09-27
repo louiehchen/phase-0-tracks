9303960a2e74277b659dfc2ac45c3c81edfe3354
Louie Chen <louiehchen@gmail.com>
2016-09-27 14:34:16 -0700

    add puppy

---
+++ ruby/puppy.rb
@@ -0,0 +1,16 @@
+=begin 
+SPECIES ------------------------
+Canis Adorableus
+
+CHARACTERISTICS ----------------
+Hair length: long
+Friendly: yes
+Eye count: 2
+Name: varies
+Color: varies
+
+BEHAVIOR -----------------------
+Drool
+Fetch
+Chase
+=end
